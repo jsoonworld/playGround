1224fa654f5c3af742049a6335c43197ac5597e4
권장순 <jsoonworld@gmail.com>
2024-07-01 11:35:01 +0900

    feat: auth settings init

---
+++ src/main/java/study/playground/domain/Part.java
@@ -0,0 +1,4 @@
+package study.playground.domain;
+
+public enum Part {
+}
---
+++ src/main/java/study/social/AuthTestMain.java
@@ -0,0 +1,10 @@
+package study.social;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTestMain {
+    public static void main(String[] args) {
+
+    }
+}
